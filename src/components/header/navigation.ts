0d98a65aad0774aba5ad202f14daac24fda9829b
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2025-08-11 14:28:47 +0500

    Add src/components/header/navigation.ts

---
+++ src/components/header/navigation.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+     { name: 'Home', href: '#home' },
+     { name: 'About', href: '#about' },
+     { name: 'Projects', href: '#projects' },
+     { name: 'Timeline', href: '#timeline' },
+     { name: 'Contact', href: '#contact' },
+];
