6fd2a100da856887d2d1352f9784bd57fff8d9e7
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2025-08-11 15:16:29 +0500

    Add src/components/projects/Decorations.tsx

---
+++ src/components/projects/Decorations.tsx
@@ -0,0 +1,6 @@
+export const BackgroundDecorations = () => (
+     <div className="absolute top-0 left-0 w-full h-full z-0">
+          <div className="absolute top-20 -left-20 w-96 h-96 bg-gradient-to-r from-blue-600/10 to-purple-600/10 rounded-full blur-3xl"></div>
+          <div className="absolute bottom-10 right-0 w-80 h-80 bg-gradient-to-r from-cyan-500/10 to-indigo-500/10 rounded-full blur-3xl"></div>
+     </div>
+);
