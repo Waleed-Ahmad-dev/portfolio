4a168c7000f44c3e73aeb53a69fa93db3d7a6f12
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-10-23 15:50:04 +0500

    Remaking the Project

--- src/components/header/navigation.ts
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-     { name: 'Home', href: '#home' },
-     { name: 'About', href: '#about' },
-     { name: 'Projects', href: '#projects' },
-     { name: 'Timeline', href: '#timeline' },
-     { name: 'Contact', href: '#contact' },
-];
